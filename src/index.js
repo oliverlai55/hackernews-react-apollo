42ec67b79e1dd28a920c40752b1789c583e9dff9
oliverlai55 <oliverlai55@gmail.com>
2017-12-06 00:39:39 -0500

    Set up Apollo client

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = new HttpLink({ uri: '__SIMPLE_API_ENDPOINT__'})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />, document.getElementById('root')
+  </ApolloProvider>
+);
+registerServiceWorker();
