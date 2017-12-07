9c5c540c433ed783050dbc911f20701b6023c7d6
oliverlai55 <oliverlai55@gmail.com>
2017-12-06 23:50:10 -0500

    Add components structure

--- src/index.js
+++ src/index.js
@@ -9,13 +9,16 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const httpLink = new HttpLink({ uri: '__SIMPLE_API_ENDPOINT__'})
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjaukoell2doy0101b3xwvht7'})
+// connect ApolloClient instance with GraphQL Api
 
+// Instantiate ApolloClient
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
 })
 
+// Higher Order Component ApolloProvider
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />, document.getElementById('root')
