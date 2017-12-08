0cbc462c97d4bd7bb6afaeea956ae2748ea893d6
oliverlai55 <oliverlai55@gmail.com>
2017-12-08 00:58:21 -0500

    Get first query to work

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import Link from './Link';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class LinkList extends Component {
 
   render() {
 
-    const linksToRender = [{
-      id: '1',
-      description: 'The Coolest GraphQL Backend',
-      url: 'https://www.graph.cool'
-    }, {
-      id: '2',
-      description: 'The Best GraphQL Client',
-      url: 'http://dev.apollodata.com/'
-    }]
+    if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+      return <div>Loading</div>;
+    }
+
+    if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+      return <div>Error</div>;
+    }
+
+    const linksToRender = this.props.allLinksQuery.allLinks;
 
     return (
       <div>
@@ -25,4 +27,15 @@ class LinkList extends Component {
   }
 }
 
-export default LinkList
+const ALL_LINKS_QUERY = gql`
+  query AllLinksQuery {
+    allLinks {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' }) (LinkList)
